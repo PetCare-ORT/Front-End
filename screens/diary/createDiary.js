6616e6a88582547d9c7039554e525537791a53a9
Eduardo Leon <eduleon94@gmail.com>
2021-11-08 23:05:32 -0300

    Update createDiary.js

--- screens/diary/createDiary.js
+++ screens/diary/createDiary.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function createDiary() {
   return (
     <View style={styles.container}>
-      <Text>Profile screen</Text>
+      <Text>Entrada de diario screen</Text>
     </View>
   );
 }
