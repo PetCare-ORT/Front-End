46567d0dd82d09acfbb43367bd602d34bed1470e
Eduardo Leon <eduleon94@gmail.com>
2021-11-08 23:06:16 -0300

    Update createReminder.js

--- screens/reminders/createReminder.js
+++ screens/reminders/createReminder.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function createReminder() {
   return (
     <View style={styles.container}>
-      <Text>Profile screen</Text>
+      <Text>Create Reminder screen</Text>
     </View>
   );
 }
