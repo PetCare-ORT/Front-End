cd0b52dee0da013960dad4df2811e904e66e3a9f
Eduardo Leon <eduleon94@gmail.com>
2021-11-08 23:05:10 -0300

    Update login.js

--- screens/mainScreens/login.js
+++ screens/mainScreens/login.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function login() {
   return (
     <View style={styles.container}>
-      <Text>Profile screen</Text>
+      <Text>Login screen</Text>
     </View>
   );
 }
