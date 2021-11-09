fa9556e27038194a46dad6094603a067a92f1e7c
Eduardo Leon <eduleon94@gmail.com>
2021-11-08 23:05:53 -0300

    Update reminders.js

--- screens/reminders/reminders.js
+++ screens/reminders/reminders.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function reminders() {
   return (
     <View style={styles.container}>
-      <Text>Profile screen</Text>
+      <Text>Reminders screen</Text>
     </View>
   );
 }
