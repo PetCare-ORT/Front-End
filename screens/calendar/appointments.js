2205556702bc94e7e9632de9741308d55a33fcc5
Julieta Sabin <64503466+sabisabi00@users.noreply.github.com>
2021-11-08 23:07:33 -0300

    Dev (#1)
    * Created a new Expo app
    
    * Creación de vistas vacías y README con alcance.
    
    * Update login.js
    
    * Update createDiary.js
    
    * Update reminders.js
    
    * Update createReminder.js
    
    * Update createPet.js
    
    Co-authored-by: Santiago Cartagena <scartagena147@gmail.com>
    Co-authored-by: MARIA JULIETA SABIN <39100964@comunidadort.edu.ar>
    Co-authored-by: Eduardo Leon <eduleon94@gmail.com>

---
+++ screens/calendar/appointments.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function turnos() {
+  return (
+    <View style={styles.container}>
+      <Text>Profile screen</Text>
+    </View>
+  );
+}
---
+++ screens/diary/createDiary.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function createDiary() {
+  return (
+    <View style={styles.container}>
+      <Text>Entrada de diario screen</Text>
+    </View>
+  );
+}
