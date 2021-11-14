3ee256b8148518b8b33bc522db4c98cb134d6c47
MARIA JULIETA SABIN <39100964@comunidadort.edu.ar>
2021-11-14 18:30:30 -0300

    Pantalla de login

--- screens/Login/LoginScreen.js
+++ screens/Login/LoginScreen.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Button,
+  TextInput,
+} from "react-native";
 import Constants from "../../lib/Constants.js";
 import * as Google from "expo-auth-session/providers/google";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { useState } from "react/cjs/react.development";
 
 export default function LoginScreen({ navigation }) {
   function login() {
     navigation.navigate(Constants.MAIN_VIEW);
   }
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const [request, response, promptAsync] = Google.useAuthRequest({
     expoClientId:
@@ -33,7 +45,44 @@ export default function LoginScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text>Login Screen</Text>
+      <Text style={styles.logo}>PetCare</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => this.setState({ email: text })}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          secureTextEntry
+          style={styles.inputText}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => this.setState({ password: text })}
+        />
+      </View>
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText} onPress={() => login()} n>
+          LOGIN
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        disabled={!request}
+        onPress={() => {
+          promptAsync().then(() => login());
+        }}
+      >
+        <MaterialCommunityIcons name="google" color={"#DB4437"} size={26} />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.loginText} onPress={() => login()}>
+          SIGNUP
+        </Text>
+      </TouchableOpacity>
+
+      {/* <Text></Text>
       <TouchableOpacity onPress={() => login()}>
         <Text> LOGIN </Text>
       </TouchableOpacity>
@@ -43,8 +92,8 @@ export default function LoginScreen({ navigation }) {
           promptAsync().then(() => login());
         }}
       >
-        <Text> LOGIN CON GOOGLE </Text>
-      </TouchableOpacity>
+        <MaterialCommunityIcons name="google" color={"#DB4437"} size={26} />
+      </TouchableOpacity> */}
     </View>
   );
 }
@@ -55,5 +104,37 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#fb5b5a",
+    marginBottom: 40,
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#465881",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: "white",
+  },
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#fb5b5a",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  loginText: {
+    color: "white",
   },
 });
