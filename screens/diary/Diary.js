c3624a76ca64be9dc6d6ff3613b299300929ab1c
MARIA JULIETA SABIN <39100964@comunidadort.edu.ar>
2021-12-05 18:12:06 -0300

    diary screen

--- screens/diary/Diary.js
+++ screens/diary/Diary.js
@@ -1,17 +1,73 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-export default function Diary() {
+import React, { useEffect, useState, useContext } from "react";
+import { View, FlatList, TouchableOpacity } from "react-native";
+import { ActivityIndicator } from "react-native-paper";
+import DiaryThumbnail from "./DiaryThumbnail.js";
+import Constants from "../../lib/Constants.js";
+import GlobalContext from "../../context";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { getUserDiaryEntry } from "../../services/diaryApi.js";
+import Styles from "../../lib/Styles.js";
+import Colors from "../../lib/Colors.js";
+
+export default function DiaryEntries({ navigation, route }) {
+  const { state, dispatch } = useContext(GlobalContext);
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getEntries = async () => {
+    try {
+      const diary = await getUserDiaryEntry();
+      const diaryData = diary.data;
+      setData(diaryData);
+      dispatch({
+        type: "STORE_DIARY_ENTRIES",
+        payload: { entries: diaryData },
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // useEffect(() => {
+  //   if (route.params.reload == true) {
+  //     getEntries();
+  //     route.params.reload = undefined;
+  //   }
+  // }, [route.params.reload]);
+
+  useEffect(() => {
+    getEntries();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Diary</Text>
+    <View style={Styles.petListContainer}>
+      {isLoading ? (
+        <ActivityIndicator size="large" color={Colors.PRIMARY_PINK} />
+      ) : (
+        <FlatList
+          columnWrapperStyle={Styles.petListColumnWrapper}
+          data={data}
+          numColumns={1}
+          keyExtractor={(item) => item._id}
+          renderItem={({ item }) => (
+            <DiaryThumbnail diaryData={item} navigation={navigation} />
+          )}
+        />
+      )}
+      <TouchableOpacity
+        style={Styles.petListAddButton}
+        onPress={() => {
+          navigation.navigate(Constants.PET_FORM_VIEW, { pet: null });
+        }}
+      >
+        <MaterialCommunityIcons
+          name="plus-circle"
+          color={Colors.PRIMARY_BLUE}
+          size={60}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ screens/diary/DiaryDetails.js
@@ -0,0 +1,148 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Alert,
+  Platform,
+} from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Constants from "../../lib/Constants.js";
+import { deleteDiary } from "../../services/diaryApi.js";
+
+export default function diaryDetail({ navigation, route }) {
+  const { diary } = route.params;
+
+  const deleteDiary = async () => {
+    try {
+      await deleteDiary(diary._id).then(() => {
+        alert("Entry deleted successfully!");
+        navigation.navigate(Constants.DIARY_VIEW, { reload: true });
+      });
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  function deleteWeb() {
+    if (confirm(Constants.ACTION_CANNOT_BE_REVERSED)) {
+      logout();
+    }
+  }
+
+  function deleteMobile() {
+    Alert.alert(
+      Constants.DELETE_CONFIRM(diary.title),
+      Constants.ACTION_CANNOT_BE_REVERSED,
+      [
+        {
+          text: "Cancel",
+          onPress: () => null,
+        },
+        {
+          text: "Confirm",
+          onPress: () => deleteDiary(),
+        },
+      ]
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
+        }}
+        style={styles.photo}
+      />
+      <Text style={styles.title}>
+        <Text>
+          Title: {diary.title}
+          {"\n"}
+        </Text>
+        <Text>
+          Description: {diary.description}
+          {"\n"}
+        </Text>
+        <Text>
+          Date: {new Date(diary.date).toLocaleDateString()}
+          {"\n"}
+        </Text>
+        <Text>Attatchment {diary.attatchment}</Text>
+      </Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          Platform.OS === "web" ? deleteWeb() : deleteMobile();
+        }}
+      >
+        <MaterialCommunityIcons
+          name="delete-circle"
+          color={"#d11515"}
+          size={60}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.editButton}
+        onPress={() => {
+          navigation.navigate(Constants.PET_FORM_VIEW, { pet: pet });
+        }}
+      >
+        <MaterialCommunityIcons
+          name="circle-edit-outline"
+          color={"#d11515"}
+          size={60}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#eaeaea",
+  },
+  title: {
+    flex: 3,
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  photo: {
+    flex: 2,
+    height: 200,
+    resizeMode: "center",
+    margin: 5,
+  },
+  button: {
+    flex: 1,
+    flexDirection: "row",
+    position: "absolute",
+    top: 10,
+    right: 5,
+    alignSelf: "flex-end",
+    justifyContent: "space-between",
+    backgroundColor: "transparent",
+  },
+  editButton: {
+    flex: 1,
+    flexDirection: "row",
+    position: "absolute",
+    top: 80,
+    right: 5,
+    alignSelf: "flex-end",
+    justifyContent: "space-between",
+    backgroundColor: "transparent",
+  },
+});
